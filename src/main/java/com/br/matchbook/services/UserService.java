9067a46114cea47f26cda3888e25ee7201949371
Bruna <bruna.lima@zup.com.br>
2019-08-07 15:10:19 -0300

    Adicionar classe service user

---
+++ src/main/java/com/br/matchbook/services/UserService.java
@@ -0,0 +1,17 @@
+package com.br.matchbook.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.matchbook.models.User;
+import com.br.matchbook.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+
+	public Iterable<User> showAllUsers() {
+		return userRepository.findAll();
+	}
+}
