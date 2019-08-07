58c2ec517162bdf4ddd64be47f2d96fd7bb9bd42
Bruna <bruna.lima@zup.com.br>
2019-08-07 15:59:43 -0300

    Adicionar validaçao de erros

--- src/main/java/com/br/matchbook/controllers/MatchControler.java
+++ src/main/java/com/br/matchbook/controllers/MatchControler.java
@@ -1,22 +1,29 @@
 package com.br.matchbook.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.br.matchbook.repositories.LiteraryGenreRepository;
-import com.br.matchbook.repositories.LoginRepository;
-import com.br.matchbook.repositories.UserRepository;
+import com.br.matchbook.models.Login;
+import com.br.matchbook.models.User;
+import com.br.matchbook.services.LoginService;
+import com.br.matchbook.services.UserService;
 
 @Controller
 public class MatchControler {
 	@Autowired
-	private LoginRepository loginRepository;
+	private LoginService loginService;
 	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-	private LiteraryGenreRepository literaryGenreRepository;
+	private UserService userService;
 	
 	@GetMapping("/")
 	public ModelAndView displayHomePage() {
@@ -34,6 +41,32 @@ public class MatchControler {
 	@GetMapping("/cadastro")
 	public ModelAndView displayRegisterPage() {
 		ModelAndView modelAndView = new ModelAndView("register.html");
+		return modelAndView;
+	}
+	
+	@PostMapping("/cadastro")
+	public ModelAndView saveForm(@Valid Login login, BindingResult bindingLogin, @Valid User user,
+			BindingResult bindingUser) {
+		ModelAndView modelAndView = new ModelAndView("register.html");
+
+		if (bindingLogin.hasErrors()  || bindingUser.hasErrors()) {
+			List<String> erros = new ArrayList<String>();
+			for (ObjectError objectError : bindingLogin.getAllErrors()) {
+				erros.add(objectError.getDefaultMessage());
+				modelAndView.addObject("login", loginService);
+				modelAndView.addObject("erros", erros);
+			}	for (ObjectError objectError : bindingUser.getAllErrors()) {
+				erros.add(objectError.getDefaultMessage());
+				modelAndView.addObject("user", userService);
+				modelAndView.addObject("erros", erros);
+			}
+		} else {
+			loginService.registerLogin(user, login);
+			modelAndView.addObject("logins", loginService.registerLogin(user, login));
+			modelAndView.addObject("users", userService.showAllUsers());
+
+		}
+		
 		return modelAndView;
 	}
 }
