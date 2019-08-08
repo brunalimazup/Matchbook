43642ef97c22735bf9157429d5a8a996256877a6
Milena Figueredo <milena.silva@zup.com.br>
2019-08-08 13:21:43 -0300

    corrigir o metodo post dos generos literarios

--- src/main/java/com/br/matchbook/controllers/MatchControler.java
+++ src/main/java/com/br/matchbook/controllers/MatchControler.java
@@ -51,9 +51,9 @@ public class MatchControler {
 		return modelAndView;
 	}
 
-	@GetMapping("/generos")
+	@GetMapping("/cadastro/genre")
 	public ModelAndView registerGenerousLiterary() {
-		ModelAndView modelAndView = new ModelAndView("genre.html");
+		ModelAndView modelAndView = new ModelAndView("registerLiteraryGenre.html");
 		return modelAndView;	
 	}
 	
